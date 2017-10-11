b3085b5ae3e91625b2c1e801ee3c5e9191a96b32
geoffcraz <geoffcraz@gmail.com>
2017-10-11 22:35:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ function takeANumber(katzDeliLine, name){
 
 }
 
-/*function nowServing(line){
-  if  line.length == 0{
-
+function nowServing(katzDeliLine){
+  if  katzDeliLine.length == 0{
+    return "There is nobody waiting to be served!"
   }
+  
 
 }
-*/
+
