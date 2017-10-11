5036f6accbbcb7d182b456d65e1e043b89a69e9c
geoffcraz <geoffcraz@gmail.com>
2017-10-11 22:40:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(line){
   if (katzDeliLine.length == 0) {
-    return "The line is empty."
+    return "The line is currently empty."
   } else {
 
     }
