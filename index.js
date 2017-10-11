4f174d8e3ed8a0f8300d7f94419ab95cf7f67df8
geoffcraz <geoffcraz@gmail.com>
2017-10-11 22:03:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function takeNumber(){
+return console.log("im doin sometin now");
+};
+
+
+function say(greeting, firstName) {
+
+    return console.log(`${greeting}, ${firstName}!`)
+}
+
+number = 12
+number++
+console.log(number)
+
+function inc(a) {
+  var c = ++a
+  console.log(c)
+return  c
+}
+
+inc(12)
+
+say("fuck you", "morgan");
+takeNumber()
