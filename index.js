ebedb8fc355dab7aed23e523504d9a8a74192b02
geoffcraz <geoffcraz@gmail.com>
2017-10-11 22:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var katzDeli = []
 
 function takeANumber(katzDeli, name){
-    var katzDeliLine = katzDeli.push(name)
-    var lineLength = katzDeliLine.length
-    return `Welcome, ${name} . You are number ${lineLength} in line.`
+    katzDeliLine = katzDeli.push(name)
+    return `Welcome, ${name} . You are number ${katzDeliLine.length} in line.`
 
 }
